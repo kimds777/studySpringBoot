7db3c0ab08bc7cf87350a3045d6d1adb916b63a1
kimds777 <77198242+kimds777@users.noreply.github.com>
2023-12-10 00:07:16 +0900

    스프링 부트 파일 업로드

---
+++ src/test/java/hellow/hellowspring/HellowSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hellow.hellowspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
