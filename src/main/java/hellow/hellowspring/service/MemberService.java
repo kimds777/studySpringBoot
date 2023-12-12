af157ae184333ba656321e91cbb1d52c35a678f2
kimds777 <77198242+kimds777@users.noreply.github.com>
2023-12-13 00:23:43 +0900

    의존 관계 설정 및 스프링 빈 등록

--- src/main/java/hellow/hellowspring/service/MemberService.java
+++ src/main/java/hellow/hellowspring/service/MemberService.java
@@ -3,14 +3,18 @@ package hellow.hellowspring.service;
 import hellow.hellowspring.domain.Member;
 import hellow.hellowspring.repository.MemberRepository;
 import hellow.hellowspring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+//@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+//    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
